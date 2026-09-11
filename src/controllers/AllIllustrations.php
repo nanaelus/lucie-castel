ccb0f4f08b48b7b9c256f663880741689d9577ff
nanaelus <anael.arnaud@laposte.net>
2026-09-11 17:22:07 +0200

    Init branch illustrations, creating controller and model AllIllustrations and Illustration

---
+++ src/controllers/AllIllustrations.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Controllers\Illustrations;
+
+require_once('src/lib/Database.php');
+
+use Application\lib\Database\DatabaseConnection;
+
+class Illustrations
+{
+    
+}
---
+++ src/model/Illustration.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Model\Illustration;
+
+use Application\Lib\Database\DatabaseConnection;
+
+require_once('src/Lib/Database.php');
+
+class Illustration
+{
+
+}
+
+class IllustrationRepository
+{
+    
+}
