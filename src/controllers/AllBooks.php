d35785a8e5ecf042cda2bbcce2c011468d9696dd
nanaelus <anael.arnaud@laposte.net>
2026-09-11 14:45:07 +0200

    Init branch books, creating controller AllBooks and template allbooks

---
+++ src/controllers/AllBooks.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Controllers\AllBooks;
+
+require_once('src/lib/Database.php');
+require_once('src/model/Book.php');
+
+use Application\lib\Database\DatabaseConnection;
+use Application\Model\Book\Book;
+use Application\Model\Book\BookRepository;
+
+class AllBooks
+{
+    
+}
