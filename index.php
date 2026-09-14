40f0565d4b5113cf75dac4ef94dc749ef23eb13f
nanaelus <anael.arnaud@laposte.net>
2026-09-14 16:28:24 +0200

    add route to the workshops

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require_once('src/controllers/Homepage.php');
 require_once('src/controllers/AllBooks.php');
 require_once('src/controllers/AllIllustrations.php');
+require_once('src/controllers/AllWorkshops.php');
 
 use Application\Controllers\Homepage\Homepage;
 use Application\Controllers\AllBooks\AllBooks;
 use Application\Controllers\AllIllustrations\AllIllustrations;
+use Application\Controllers\AllWorkshops\AllWorkshops;
 
 try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
@@ -15,7 +17,10 @@ try {
         }
     } elseif (isset($_GET['toutes-mes-illustrations'])) {
         (new AllIllustrations())->execute();
-    } else {
+    } elseif (isset($_GET['tous-mes-ateliers'])) {
+        (new AllWorkshops())->execute();
+    }
+    else {
         (new Homepage())->execute();
     }
 } catch (Exception $e) {
