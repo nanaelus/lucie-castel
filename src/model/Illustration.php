c198f9a0d639d1e2e6ce82ed1e0a2ec0c476120f
nanaelus <anael.arnaud@laposte.net>
2026-09-14 14:43:05 +0200

    add construct to Illustration Object, add function to get all items in the database

--- src/model/Illustration.php
+++ src/model/Illustration.php
@@ -10,10 +10,56 @@ require_once('src/Lib/Database.php');
 
 class Illustration
 {
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $date,
+        private string $description
+        ) {}
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
 }
 
 class IllustrationRepository
 {
-    
+    public DatabaseConnection $connection;
+
+    public function getIllustration(): ?array
+    {
+        $statement = $this->connection->getConnection()->query(
+            "SELECT id, name, DATE_FORMAT(date, '%d-%m-%y') AS french_date, description FROM illustration"
+        );
+        $illustrations = [];
+        while ($row = $statement->fetch()) {
+            $illustration = new Illustration(
+                $row['id'],
+                $row['name'],
+                $row['french_date'],
+                $row['description']
+            );
+            $illustrations[] = $illustration;
+        }
+        if (empty($illustrations)) {
+            return null;
+        }
+        return $illustrations;
+    }
 }
